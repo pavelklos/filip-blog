6f2162f257c4dc9ee2ba4e5c967df45184b0c942
pavelklos <pavelkloscz@gmail.com>
2021-06-09 10:12:08 +0200

    Fonts + minor styling improvements

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -1,23 +1,37 @@
 // _rfc
 import { Container } from "react-bootstrap";
+import Head from "next/head";
 import BlogNavbar from "components/Navbar";
 
 export default function PageLayout(props) {
   const { children, className } = props;
 
   return (
-    <Container>
-      <BlogNavbar />
-      <div className={`page-wrapper ${className}`}>{children}</div>
-      <footer className='page-footer'>
-        <div>
-          <a href='#'>courses</a>
-          {" | "}
-          <a href='#'>github</a>
-          {" | "}
-          <a href='#'>facebook</a>
-        </div>
-      </footer>
-    </Container>
+    <>
+      <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        {/* <link
+          href='https://fonts.googleapis.com/css2?family=Nunito:wght@400;700;900&display=swap'
+          rel='stylesheet'
+        /> */}
+        <link
+          href='https://fonts.googleapis.com/css2?family=Nunito:wght@300;700;900&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
+      <Container>
+        <BlogNavbar />
+        <div className={`page-wrapper ${className}`}>{children}</div>
+        <footer className='page-footer'>
+          <div>
+            <a href='#'>courses</a>
+            {" | "}
+            <a href='#'>github</a>
+            {" | "}
+            <a href='#'>facebook</a>
+          </div>
+        </footer>
+      </Container>
+    </>
   );
 }
