5d138062443082e48ce4939778eedd483d22962d
pavelklos <pavelkloscz@gmail.com>
2021-06-10 16:35:00 +0200

    getStaticPaths function

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -10,6 +10,7 @@ export default function BlogDetailPage(props) {
   const { blog } = props;
   // debugger;
   // console.log(blog);
+  console.log("Displaying page for slug:", blog?.slug);
 
   return (
     <PageLayout>
@@ -18,15 +19,43 @@ export default function BlogDetailPage(props) {
   );
 }
 
-export async function getServerSideProps(context) {
-  console.log("[[slug].js] Calling ... `getServerSideProps() : λ (Server)");
+export async function getStaticPaths() {
+  console.log(
+    "[[slug].js] Calling ... `getStaticPaths() : Gettings paths for every page"
+  );
+  return {
+    paths: [
+      { params: { slug: "my-first-blog" } },
+      { params: { slug: "my-second-blog" } },
+      { params: { slug: "my-third-blog" } },
+    ],
+    fallback: false, // false -> 404
+  };
+}
+
+export async function getStaticProps(context) {
+  console.log("[[slug].js] Calling ... `getStaticProps() : ● (SSG) DYNAMIC");
   const { params } = context;
   const { slug } = params;
 
   const blog = await getBlogBySlug2(slug);
+  console.log("Fetching blog by slug:", slug);
   return {
     props: {
       blog,
     },
   };
 }
+
+// export async function getServerSideProps(context) {
+//   console.log("[[slug].js] Calling ... `getServerSideProps() : λ (Server)");
+//   const { params } = context;
+//   const { slug } = params;
+
+//   const blog = await getBlogBySlug2(slug);
+//   return {
+//     props: {
+//       blog,
+//     },
+//   };
+// }
