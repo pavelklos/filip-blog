330553b959139456e971aeff0c6a4d816b57b60e
pavelklos <pavelkloscz@gmail.com>
2021-06-11 22:31:16 +0200

    Updated : highlight : registerLanguage

--- components/HighlightCode.js
+++ components/HighlightCode.js
@@ -1,13 +1,19 @@
 // _rfc
 import { createRef, useEffect, useRef } from "react";
 import { findDOMNode } from "react-dom";
-import highlight from "highlight.js";
+// https://highlightjs.org/usage/
+// import highlight from "highlight.js";
+import hljs from "highlight.js/lib/core";
+import javascript from "highlight.js/lib/languages/javascript";
+import html from "highlight.js/lib/languages/xml";
+hljs.registerLanguage("javascript", javascript);
+hljs.registerLanguage("html", html);
 
 export default function HighlightCode({ children, language }) {
   const codeRef = createRef();
 
   useEffect(() => {
-    highlight.highlightBlock(findDOMNode(codeRef.current));
+    hljs.highlightBlock(findDOMNode(codeRef.current));
   }, []); // ON INITIAL RENDER
 
   return (
