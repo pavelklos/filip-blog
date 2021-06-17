036bdaa1391f2166e0a7f0e475af9c29aec71044
pavelklos <pavelkloscz@gmail.com>
2021-06-17 10:09:45 +0200

    Fetch more blogs

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,8 +2,11 @@
 import { getAllBlogs } from "lib/api";
 
 export default async function getBlogs(req, res) {
+  const offset = parseInt(req.query.offset || 0, 10);
+
   // res.status(200).json([1, 2, 3, 4, 5]); // test
-  const data = await getAllBlogs(); // data = blogs array
+  // const data = await getAllBlogs(); // data = blogs array
   // const data = await getAllBlogs({ offset: 0 }); // data = blogs array
+  const data = await getAllBlogs({ offset });
   res.status(200).json(data);
 }
